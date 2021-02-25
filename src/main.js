b40570e5cea4a6e00500f15416ebd90c67f27e7f
jeonghun <sjh960612@gmail.com>
2021-02-25 23:32:32 +0900

    init

--- src/main.js
+++ src/main.js
@@ -2,11 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import io from "socket.io-client";
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
+const socket = io.connect("http://192.168.219.150:50000", {
+  reconnect: true,
+  transports: ["websocket"],
+});
+Vue.prototype.$socket = socket
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
